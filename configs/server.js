f8d40f7021327809dd237e445ddb098fc879ecdb
Zodiacal0 <javierherrera5513@gmail.com>
2025-07-13 21:52:51 -0600

    fix(cors): Acces control

--- configs/server.js
+++ configs/server.js
@@ -16,19 +16,32 @@ import transactionRoutes from "../src/transaction/transaction.routes.js";
 import { userSeeder } from "../src/seeders/user.seeder.js";
 
 const middlewares = (app) => {
+  const allowedOrigins = [
+    "http://localhost:5173",
+    "https://deploy-front-bank.web.app"
+  ];
+  
+  app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("No permitido por CORS"), false);
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  }));
+  app.options("*", cors());
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
   app.use(helmet());
-  app.use(cors({
-    origin: "*",            
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-  }));
-  app.options("*", cors());
   app.use(morgan("dev"));
   app.use(cookieParser());
 };
+
 
 const routes = (app) => {
   app.use("/walletManager/v1/auth", authRoutes);

6a7bd0e1c3e428433c44a7ac2fc389b74e22f9e0
Zodiacal0 <javierherrera5513@gmail.com>
2025-07-14 16:50:49 -0600

    feat(test): test 2

--- src/middlewares/validate-token.js
+++ src/middlewares/validate-token.js
@@ -5,7 +5,7 @@ export const validateJWT = async (req, res, next) => {
   try {
     // Leer token desde la cookie o desde headers/body/query
     let token =
-      req.cookies?.auth?.token ||
+      req.cookies?.auth_token || // ✅ Cambié a auth_token
       req.body.token ||
       req.query.token ||
       req.headers["authorization"];
@@ -36,6 +36,57 @@ export const validateJWT = async (req, res, next) => {
     return res.status(500).json({
       success: false,
       message: "Error at validate token",
+      error: error.message,
+    });
+  }
+};
+
+export const validateTokenResponse = async (req, res) => {
+  try {
+    let token =
+      req.cookies?.auth_token ||
+      req.body.token ||
+      req.query.token ||
+      req.headers["authorization"];
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Token not found",
+      });
+    }
+
+    token = token.replace(/^Bearer\s+/, "");
+    const { uid } = jwt.verify(token, process.env.SECRET_OR_PRIVATE_KEY);
+    const user = await User.findById(uid);
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "The user doesn't exist in the database",
+      });
+    }
+
+    if (user.status === false) {
+      return res.status(401).json({
+        success: false,
+        message: "User deactivated previously",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Token is valid",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid or expired token",
       error: error.message,
     });
   }
--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { getUserById, getUsersTransaction, getWallet, getBalance, getUsers, updatePassword, updateUser, deleteUser, getHistory, addFavorite, getFavorites } from "./user.controller.js";
 import { getUserByIdValidator, updatePasswordValidator, updateUserValidator, deleteUserValidator, addFavoriteValidator, getFavoritesValidator } from "../middlewares/user-validators.js";
-import { validateJWT } from "../middlewares/validate-token.js";
+import { validateTokenResponse } from "../middlewares/validate-token.js";
 const router = Router();
 
 /**
@@ -232,7 +232,7 @@ router.get(
 
 router.get(
     "/validateToken",
-    validateJWT
+    validateTokenResponse
 )
 
 export default router;
